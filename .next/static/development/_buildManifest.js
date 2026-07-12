fdc3edf2513bd39871ba3224d6cbd148fdb13f2c
sanidhyamehra <sanidhyamehra36@gmail.com>
2026-07-12 16:52:37 +0530

    @update@

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {__rewrites:{afterFiles:[{has:a,source:"\u002Fsitemap.xml",destination:"\u002Fapi\u002Fsitemap.xml"},{has:a,source:"\u002Frobots.txt",destination:"\u002Fapi\u002Frobots.txt"}],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]}}(void 0));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
